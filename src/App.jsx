5113ad6c05a00a1071056507573e4b4bdc5e2b89
nur <nurchowdhury56@gmail.com>
2025-10-20 13:39:23 +0600

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
 import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -9,6 +10,8 @@ export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <div className=' h-dvh' />
     </div>
   )
 }
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,107 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
+import { SplitText } from 'gsap/all';
+import React, { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive';
+
+export default function Hero() {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({maxWidth: 767});
+
+    useGSAP(() => {
+        const heroSplit = new SplitText('.title', { type: 'chars, words' });
+
+        const paragraphSplit = new SplitText('.subtitle', { type: 'lines' });
+        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+        
+        gsap.from(heroSplit.chars, {
+            yPercent: 100,
+            duration: 1.8,
+            ease: 'expo.out',
+            stagger: 0.07,
+        });
+
+        gsap.from(paragraphSplit.lines, {
+            opacity: 0,
+            yPercent: 100,
+            duration: 1.5,
+            ease: 'expo.out',
+            stagger: 0.07,
+            delay: 1,
+        })
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '#hero',
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+            }
+        })
+        .to('.right-leaf', { y: 200}, 0)
+        .to('.left-leaf', { y: -200}, 0)
+        .to(".arrow", { y: 100 }, 0);
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%';
+        const endValue = isMobile ? '120% 50%' : 'bottom top';
+
+        let timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        });
+        videoRef.current.onloadedmetadata = () => {
+            timeline.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        }
+    }, [])
+
+    return (
+        <div>
+            <section id="hero" className=' noisy'>
+                <h1 className=' title'> MOJITO </h1>
+                <img 
+                    src='/images/hero-left-leaf.png'
+                    alt='left-leaf'
+                    className=' left-leaf'
+                />
+                <img 
+                    src='/images/hero-right-leaf.png'
+                    alt='right-leaf'
+                    className=' right-leaf'
+                />
+                <div className=' body'>
+                    <div className=' content'>
+                        <div className=' space-y-5 hidden md:block'>
+                            <p className=' my-4'>Cool. Crisp. Classic.</p>
+                            <p className=' subtitle'>Sip the Spirit<br /> of Summer</p>
+                        </div>
+                        <div className=' view-cocktails'>
+                            <p className=' subtitle'>
+                                Every cocktail on our menu is a blend of premium spirits, fresh 
+                                ingredients, and a dash of passion - designed to delight your senses.
+                            </p>
+                            <a href='#cocktails'>View Cocktails</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div className=' video absolute inset-0'>
+                <video 
+                    ref={videoRef}
+                    src='/videos/output.mp4'
+                    muted
+                    playsInline
+                    preload='auto'
+                />
+
+            </div>
+        </div>
+    )
+}
