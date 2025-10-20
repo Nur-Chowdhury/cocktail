1937e3add4813db800347a0cf7494a8b324e5e1b
nur <nurchowdhury56@gmail.com>
2025-10-20 11:32:54 +0600

    navbar and resources

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
+import Navbar from './Components/Navbar'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 export default function App() {
   return (
     <div>
-      hi
+      <Navbar />
     </div>
   )
 }
